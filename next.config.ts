a6fd624df28e3afa1e5e2dec1f1a996c5e4584b3
Stephen <stephenvijay093@gmail.com>
2025-07-21 12:18:33 +0530

    deployment fixes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+  images: {
+    domains: ["flowbite.com","res.cloudinary.com"], // Add the domain here  
+    unoptimized: true,
+  },
+  output: "export",
+  rules: {
+    'react/no-unescaped-entities': 'off',
+    '@next/next/no-page-custom-font': 'off',
+  },  
+  // else  
+  reactStrictMode: false,
 };
 
-export default nextConfig;
+export default nextConfig;  
